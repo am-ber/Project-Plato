29e012840508295e7fc900bdd1ad6e34b4b758e6
R. Jude Stockstill <31357070+rjstockstill@users.noreply.github.com>
2019-02-28 22:28:36 -0600

    Add files via upload
    Also has a worthless Swing browser. Should be thrown into a fire but leaving it for 𝒹𝑜𝒸𝓊𝓂𝑒𝓃𝓉𝒶𝓉𝒾𝑜𝓃

---
+++ Scraper/src/cs410/Main.java
@@ -0,0 +1,29 @@
+package cs410;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    
+    Scraper scrape = new Scraper();
+    
+    String filepath = "D:\\Eclipse\\eclipse-workspace\\CS410_Project\\src\\cs410\\index2.html";
+    String[] lines = scrape.readInputHTML(Paths.get(filepath));
+    
+    //Prints contents of file
+    for(int i = 0; i < lines.length; i++) {
+      //System.out.println(lines[i]);
+    }
+    
+    List<String[]> identifiers = scrape.getIdentifiers(lines, "all");
+    for(int i = 0; i < identifiers.size(); i++) {
+      System.out.println(identifiers.get(i)[0] + ", Line:" + identifiers.get(i)[1] + ", Pos:" + identifiers.get(i)[2]);
+    }
+    
+    //new WebBrowser();
+  }
+
+}
