720ee5966777bba2d4331273d431a12761b7f08e
Zachary Vanscoit <zachary816@gmail.com>
2019-03-18 13:06:13 -0500

    Added Scrapper to tools
    Also removed Jude's Web browser with swing because JavaFX is better.

---
+++ JavaWebBrowser/src/tools/Scraper.java
@@ -0,0 +1,96 @@
+package tools;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scraper {
+
+	public Scraper() {
+		// constructor
+	}
+
+	public String[] readInputHTML(Path path) throws IOException {
+		List<String> lines = Files.readAllLines(path, Charset.forName("UTF-8"));
+		String[] arr = lines.toArray(new String[lines.size()]);
+		return arr;
+	}
+
+	public List<String[]> getIdentifiers(String[] html, String category) {
+		List<String[]> identifiers = new ArrayList<String[]>();
+
+		for (int i = 0; i < html.length; i++) {
+
+			if (category == "all") {
+				if (html[i].contains("<input") || html[i].contains("<button")) {
+					int pos = html[i].indexOf("id=\"");
+					int startingPos = pos;
+					if (pos != -1) {
+						List<Character> chars = new ArrayList<Character>();
+						while (html[i].charAt(pos + 4) != '\"') {
+							chars.add(html[i].charAt(pos + 4));
+							pos++;
+						}
+						StringBuilder sb = new StringBuilder();
+						for (Character ch : chars) {
+							sb.append(ch);
+						}
+						String string = sb.toString();
+						String[] tempArr = new String[] { string, Integer.toString(i), Integer.toString(startingPos) };
+						identifiers.add(tempArr);
+					}
+				}
+			}
+
+			if (category == "input") {
+				if (html[i].contains("<input")
+						&& (html[i].contains("type=\"text\"") || html[i].contains("type=\"password\""))) {
+					int pos = html[i].indexOf("id=\"");
+					int startingPos = pos;
+					if (pos != -1) {
+						List<Character> chars = new ArrayList<Character>();
+						while (html[i].charAt(pos + 4) != '\"') {
+							chars.add(html[i].charAt(pos + 4));
+							pos++;
+						}
+						StringBuilder sb = new StringBuilder();
+						for (Character ch : chars) {
+							sb.append(ch);
+						}
+						String string = sb.toString();
+						String[] tempArr = new String[] { string, Integer.toString(i), Integer.toString(startingPos) };
+						identifiers.add(tempArr);
+					}
+				}
+			}
+
+			if (category == "button") {
+				if ((html[i].contains("<input") && html[i].contains("type=\"button\""))
+						|| (html[i].contains("<button"))) {
+					int pos = html[i].indexOf("id=\"");
+					int startingPos = pos;
+					if (pos != -1) {
+						List<Character> chars = new ArrayList<Character>();
+						while (html[i].charAt(pos + 4) != '\"') {
+							chars.add(html[i].charAt(pos + 4));
+							pos++;
+						}
+						StringBuilder sb = new StringBuilder();
+						for (Character ch : chars) {
+							sb.append(ch);
+						}
+						String string = sb.toString();
+						String[] tempArr = new String[] { string, Integer.toString(i), Integer.toString(startingPos) };
+						identifiers.add(tempArr);
+					}
+				}
+			}
+
+		}
+		return identifiers;
+	}
+
+}
--- Scraper/src/cs410/Main.java
+++ Scraper/src/cs410/Main.java
@@ -6,24 +6,26 @@ import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    
-    Scraper scrape = new Scraper();
-    
-    String filepath = "D:\\Eclipse\\eclipse-workspace\\CS410_Project\\src\\cs410\\index2.html";
-    String[] lines = scrape.readInputHTML(Paths.get(filepath));
-    
-    //Prints contents of file
-    for(int i = 0; i < lines.length; i++) {
-      //System.out.println(lines[i]);
-    }
-    
-    List<String[]> identifiers = scrape.getIdentifiers(lines, "all");
-    for(int i = 0; i < identifiers.size(); i++) {
-      System.out.println(identifiers.get(i)[0] + ", Line:" + identifiers.get(i)[1] + ", Pos:" + identifiers.get(i)[2]);
-    }
-    
-    //new WebBrowser();
-  }
+	public static void main(String[] args) throws IOException {
+
+		Scraper scrape = new Scraper();
+
+		String filepath = "src/cs410/index2.html";
+		filepath = "https://www.google.com";
+		String[] lines = scrape.readInputHTML(Paths.get(filepath));
+
+		// Prints contents of file
+		for (int i = 0; i < lines.length; i++) {
+			// System.out.println(lines[i]);
+		}
+
+		List<String[]> identifiers = scrape.getIdentifiers(lines, "all");
+		for (int i = 0; i < identifiers.size(); i++) {
+			System.out.println(
+					identifiers.get(i)[0] + ", Line:" + identifiers.get(i)[1] + ", Pos:" + identifiers.get(i)[2]);
+		}
+
+		// new WebBrowser();
+	}
 
 }
