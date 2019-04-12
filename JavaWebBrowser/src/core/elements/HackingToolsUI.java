0af268d2f5f82258b21f9d538f37074a5bd5bea7
Zachary Vanscoit <zachary816@gmail.com>
2019-04-12 15:35:04 -0500

    began adding hackerUI

--- JavaWebBrowser/src/core/elements/HackingToolsUI.java
+++ JavaWebBrowser/src/core/elements/HackingToolsUI.java
@@ -1,13 +1,26 @@
 package core.elements;
 
 import core.JavaBrowserLauncher;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import tools.CP;
 
 public class HackingToolsUI extends GridPane {
 	
 	@SuppressWarnings("unused")
 	private JavaBrowserLauncher launcher;
+	
+	private Button tautologyAttackButton;
+	private Button secondOrderAttackButton;
+	private Button attack3Button;
+	private Button attack4Button;
+	private Button attack5Button;
+	
+	private Label menuLabel;
 	
 	public HackingToolsUI(JavaBrowserLauncher launcher) {
 		this.launcher = launcher;
@@ -16,5 +29,54 @@ public class HackingToolsUI extends GridPane {
 		setVgap(5);
 		setHgap(5);
 		
+		menuLabel = new Label("Hacker Man");
+		add(menuLabel, 0, 0);
+	}
+	
+	public void initButtons() {
+		tautologyAttackButton = new Button("Tautology Attack");
+		tautologyAttackButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				CP.println("");
+			}
+		});
+		add(tautologyAttackButton, 0, 1);
+		
+		secondOrderAttackButton = new Button("Second Order");
+		secondOrderAttackButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				CP.println("");
+			}
+		});
+		add(secondOrderAttackButton, 0, 2);
+		
+		attack3Button = new Button("the 3rd?");
+		attack3Button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				CP.println("");
+			}
+		});
+		add(attack3Button, 0, 3);
+		
+		attack4Button = new Button("4th ??");
+		attack4Button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				CP.println("");
+			}
+		});
+		add(attack4Button, 0, 4);
+		
+		attack5Button = new Button("5 ?");
+		attack5Button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				CP.println("");
+			}
+		});
+		add(attack5Button, 0, 5);
 	}
 }
